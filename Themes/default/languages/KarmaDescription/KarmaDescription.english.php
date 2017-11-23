46660f16d18c3ccb18f26e2093e4d53f63fb4a98
realdigger <neo@samtel.ru>
2017-11-23 12:22:03 +0400

    Move languages to separate files.

--- Themes/default/languages/KarmaDescription/KarmaDescription.english.php
+++ Themes/default/languages/KarmaDescription/KarmaDescription.english.php
@@ -1,27 +1,23 @@
-<?xml version="1.0"?>
-<!DOCTYPE modification SYSTEM "http://www.simplemachines.org/xml/modification">
-<modification xmlns="http://www.simplemachines.org/xml/modification" xmlns:smf="http://www.simplemachines.org/">
-	<id>IvanV.PetrovichAkaGrek:KarmaDescriptionMod</id>
-	<version>2.7.1</version>
-		<file name="$themedir/languages/ManageMaintenance.english.php" error="skip">
-		<operation>
-			<search position="end"></search>
-			<add><![CDATA[
-//Karma Descriprion Mod
+<?php
+/**
+ * @package SMF Karma Description
+ * @author Grek.Kamchatka
+ * @copyright 2005—2017 Grek.Kamchatka
+ * @license GPLv3
+ * @file KarmaDescription.english-utf8.php
+ * @version 2.8
+ */
+
+global $helptxt, $settings, $context, $scripturl;
+
+// ManageMaintenance
 $txt['maintain_karmalog'] = 'Empty out Karma Description log';
 $txt['maintain_karmalog_info'] = 'Erase all karma description log';
 $txt['maintain_karma_points'] = 'Set all karma points to 0 to all members';
 $txt['maintain_karma_points_info'] = 'Set all karma points to 0 to all members';
 $txt['maintain_karma_points_info2'] = 'and erase all karma description log';
-]]></add>
-		</operation>
-	</file>
-	<file name="$themedir/languages/ManageSettings.english.php" error="skip">
-		<operation>
-			<search position="end"></search>
-			<add><![CDATA[
-//Karma Descriprion Mod
-global $settings;
+
+// ManageSettings
 $txt['karmadescmod'] = 'Enable Karma Description Mod';
 $txt['karmamaxmembers'] = 'Amount of lines per one page in Karma Description log';
 $txt['karmalogview'] = 'Use member names as link to their profiles';
@@ -41,8 +37,8 @@ $txt['karma_pm_send_changelink'] = 'Send link in personal message to message whe
 $txt['karma_pm_send_link'] = 'Send link in personal message to own karma log';
 $txt['karmacantmodify'] = 'User ID\'s, whose karma can\'t be changed. <div class="smalltext">Comma separated, without spaces. For example: 1,13,27</div>';
 $txt['karmacantmodify2'] = 'User ID\'s, who can\'t modify karma. <div class="smalltext">For example: 2,412,88</div>';
-$txt['karmabuttons'] = 'Show small images instead of karma applaud and smite text labels <img src="'.$settings['default_images_url'].'/kdm_up.png" /><img src="'.$settings['default_images_url'].'/kdm_down.png" />';
-$txt['karmapictureinlog'] = 'Show small images in karma description log instead of + or - in action field <img src="'.$settings['default_images_url'].'/kdm_up.png" /><img src="'.$settings['default_images_url'].'/kdm_down.png" />';
+$txt['karmabuttons'] = 'Show small images instead of karma applaud and smite text labels <img src="' . $settings['default_images_url'] . '/kdm_up.png" /><img src="' . $settings['default_images_url'] . '/kdm_down.png" />';
+$txt['karmapictureinlog'] = 'Show small images in karma description log instead of + or - in action field <img src="' . $settings['default_images_url'] . '/kdm_up.png" /><img src="' . $settings['default_images_url'] . '/kdm_down.png" />';
 $txt['karmadelete'] = 'Delete user karma points when i delete descriptions of changing in Karma Description log';
 $txt['karmalabellink'] = 'Use karma label as link to members karma description log';
 $txt['karmalastchange'] = 'Enable last karma change on BoardIndex';
@@ -51,24 +47,12 @@ $txt['karmadescfieldnum'] = 'How much symbols will be in Description field on Bo
 $txt['karmaremain'] = 'Amount of karma, which user can put for 24h <div class="smalltext">0 = no limit</div>';
 $txt['karmainmessage'] = 'Display karma points in messages that users has got for ther messages';
 $txt['karmainmessagelink'] = 'Allow users to watch karma changes for concrete message';
-]]></add>
-		</operation>
-	</file>
-	<file name="$themedir/languages/Who.english.php" error="skip">
-		<operation>
-			<search position="end"></search>
-			<add><![CDATA[
-//Karma Descriprion Mod
+
+// Who
 $txt['whoadmin_viewkarma'] = 'Viewing <a href="' . $scripturl . '?action=viewkarma">Karmachange Description log</a>.';
 $txt['whoall_modifykarma'] = 'Changing karma.';
-]]></add>
-		</operation>
-	</file>
-	<file name="$themedir/languages/Help.english.php" error="skip">
-		<operation>
-			<search position="end"></search>
-			<add><![CDATA[
-//Karma Descriprion Mod
+
+// Help
 $helptxt['karmapermiss'] = 'By default this option is switched off and Karma Log can look only admins. If you want to do Karma Log available for other users or moderators, etc, enable this option and give the right that group, which can look Log in "Permissions" section.';
 $helptxt['karmaotherstat'] = 'Show other karma statistics like Max applauded users or Max smited users and so on in whole karma log.';
 $helptxt['karmalinks'] = 'If you enable this feauture, users will see two links in their profiles (Own or other users). They can see who change their karma and whom they change it. Karma Description Mod must be enabled';
@@ -84,14 +68,7 @@ $helptxt['karmacantmodify'] = 'Comma separated, without any spaces.';
 $helptxt['karmacantmodify2'] = 'Comma separated, without any spaces.';
 $helptxt['karmaremain'] = '0 - no limit';
 
-]]></add>
-		</operation>
-	</file>
-	<file name="$themedir/languages/index.english.php" error="skip">
-		<operation>
-			<search position="end"></search>
-			<add><![CDATA[
-//Karma Description Mod
+// index
 $txt['karmaview'] = 'Karma Change Description';
 $txt['whykarmamod'] = 'Karma change';
 $txt['Description'] = 'No Description';
@@ -132,54 +109,38 @@ $txt['karma_notifier'] = 'Your karma was changed on ';
 $txt['karma_notifier2'] = ' points. Do you want to see the karma description log?';
 $txt['karma_pm_subject'] = 'Karma Notification';
 $txt['karma_pm_body'] = 'Dear user. Your karma was changed. ';
-$txt['karma_pm_body2'] = 'You can see the log if follow this link:<br />'.$scripturl.'?action=ownkarma;u='.'';
+$txt['karma_pm_body2'] = 'You can see the log if follow this link:<br />' . $scripturl . '?action=ownkarma;u=' . '';
 $txt['karma_pm_send_desc'] = 'Send the reason in personal message';
 $txt['karma_pm_send_desc2'] = '<br /><br />The reason of changing was: ';
 $txt['karma_pm_send_changelink'] = '<br /><br />Your karma was changed on this message:<br />';
-$txt['karma_last_change']='Last karma change';
-$txt['karma_last_change1']='Last ';
-$txt['karma_last_change2']=' karma changes';
-$txt['karma_remain']='Amount of karma points: ';
-$txt['karma_remain_admin']='&infin;';
-$txt['karma_count']=' karm for this message';
+$txt['karma_last_change'] = 'Last karma change';
+$txt['karma_last_change1'] = 'Last ';
+$txt['karma_last_change2'] = ' karma changes';
+$txt['karma_remain'] = 'Amount of karma points: ';
+$txt['karma_remain_admin'] = '&infin;';
+$txt['karma_count'] = ' karm for this message';
 
-$txt['karmamessage']='Karma reasons for concrete message';
-$txt['karmamessage_executor']='Changed';
-$txt['karmamessage_description']='Reason';
-$txt['karmamessage_date']='Date';
-$txt['karmamessage_what']='Change';
-$txt['karmamessage_author']='Message';
-]]></add>
-		</operation>
-	</file>
-	<file name="$themedir/languages/ManagePermissions.english.php" error="skip">
-		<operation>
-			<search position="end"></search>
-			<add><![CDATA[
+$txt['karmamessage'] = 'Karma reasons for concrete message';
+$txt['karmamessage_executor'] = 'Changed';
+$txt['karmamessage_description'] = 'Reason';
+$txt['karmamessage_date'] = 'Date';
+$txt['karmamessage_what'] = 'Change';
+$txt['karmamessage_author'] = 'Message';
+
+// ManagePermissions
 $txt['permissionname_karmalog_view'] = 'View Karma Log';
 $txt['permissionhelp_karmalog_view'] = 'Karma Description Log shows all members karma and description of applauding or smiting. This permission will only work if you also have enabled it in \'Features and Options\'. You can access the Karma Description screen by clicking the link in the \'Karma Stat\' section of the board index.';
-]]></add>
-		</operation>
-	</file>
-	<file name="$themedir/languages/Errors.english.php" error="skip">
-		<operation>
-			<search position="end"></search>
-			<add><![CDATA[
+
+// Errors
 $txt['cannot_karmalog_view'] = 'Sorry - you don\'t have the proper permissions to view the Karma Description list.';
 $txt['viewkarma_error'] = 'User ID must be a number';
 $txt['karma_not_topic_starter'] = 'Sorry, he(she) is not a topic\'s starter.';
 $txt['karma_cant_modify'] = 'Sorry, you can\'t modify karma to this user.';
 $txt['karma_cant_modify2'] = 'Sorry, you can\'t modify karma to other users.';
 $txt['kdm_error'] = 'Karma Description Mod is disabled';
-global $context;
-$txt['kdm_remain_error'] = 'Sorry, you have reached a 24h limit of karma changes. You can change only '.isset($modSettings['karmaremain']).' times in 24 h. <br>Next karma change will be available on '.isset($context['nextKarma']).'';
-]]></add>
-		</operation>
-	</file>
-	<file name="$themedir/languages/Profile.english.php" error="skip">
-		<operation>
-			<search position="end"></search>
-			<add><![CDATA[
+$txt['kdm_remain_error'] = 'Sorry, you have reached a 24h limit of karma changes. You can change only ' . isset($modSettings['karmaremain']) . ' times in 24 h. <br>Next karma change will be available on ' . isset($context['nextKarma']) . '';
+
+// Profile
 $txt['who_change_my_karma'] = 'Who changed my karma';
 $txt['whom_i_change_karma'] = 'Whose karma have I changed';
 $txt['enable_notify'] = 'Notify me when my karma will be changed';
@@ -187,7 +148,3 @@ $txt['enable_notify_none'] = 'don\'t notify';
 $txt['enable_notify_popup'] = 'Popup window';
 $txt['enable_notify_pm'] = 'Personal Message';
 $txt['display_karma_settings'] = 'Change Karma notify';
-]]></add>
-		</operation>
-	</file>
-</modification>
--- Themes/default/languages/KarmaDescription/KarmaDescription.russian.php
+++ Themes/default/languages/KarmaDescription/KarmaDescription.russian.php
@@ -1,28 +1,23 @@
-<?xml version="1.0"?>
-<!DOCTYPE modification SYSTEM "http://www.simplemachines.org/xml/modification">
-<modification xmlns="http://www.simplemachines.org/xml/modification" xmlns:smf="http://www.simplemachines.org/">
-	<id>IvanV.PetrovichAkaGrek:KarmaDescriptionMod</id>
-	<version>2.7.1</version>
-		<file name="$themedir/languages/ManageMaintenance.russian.php" error="skip">
-		<operation>
-			<search position="end"></search>
-			<add><![CDATA[
-//Karma Descriprion Mod
+<?php
+/**
+ * @package SMF Karma Description
+ * @author Grek.Kamchatka
+ * @copyright 2005—2017 Grek.Kamchatka
+ * @license GPLv3
+ * @file KarmaDescription.russian.php
+ * @version 2.8
+ */
+
+global $helptxt, $settings, $context, $scripturl;
+
+// ManageMaintenance
 $txt['maintain_karmalog'] = 'Очистить лог кармы';
 $txt['maintain_karmalog_info'] = 'Удалить все описания изменений кармы';
 $txt['maintain_karma_points'] = 'Сбросить карму всех пользователей на ноль';
 $txt['maintain_karma_points_info'] = 'Данная опция сбрасывает все значения кармы на ноль у каждого пользователя';
-$txt['maintain_karma_points_info2'] = 'а так же удалить все описания изменений кармы';
-]]></add>
-		</operation>
-	</file>
+$txt['maintain_karma_points_info2'] = 'а, также, удалить все описания изменений кармы';
 
-	<file name="$themedir/languages/ManageSettings.russian.php" error="skip">
-		<operation>
-			<search position="end"></search>
-<add><![CDATA[
-//Karma Descriprion Mod
-global $settings;
+// ManageSettings
 $txt['karmadescmod'] = 'Включить Karma Description Mod';
 $txt['karmamaxmembers'] = 'Количество строк на одну страницу в логе кармы';
 $txt['karmalogview'] = 'Использовать имена пользователей как ссылки в профиль';
@@ -42,9 +37,9 @@ $txt['karma_pm_send_changelink'] = 'Отправлять ссылку в личном сообщение на сооб
 $txt['karma_pm_send_link'] = 'Отправлять ссылку в личном сообщении на собственный лог кармы';
 $txt['karmacantmodify'] = 'ID пользователей, чья карма не может быть изменена. <div class="smalltext">Через запятую, без пробелов, например: 1,13,27</div>';
 $txt['karmacantmodify2'] = 'ID пользователей, которые не могут изменять карму. <div class="smalltext">Например: 2,412,88</div>';
-$txt['karmabuttons'] = 'Показывать небольшие кнопки вместо текстовых названий изменения кармы <img src="'.$settings['default_images_url'].'/kdm_up.png" /><img src="'.$settings['default_images_url'].'/kdm_down.png" />';
-$txt['karmapictureinlog'] = 'Показывать небольшое изображение в логе кармы, вместо плюсов и минусов <img src="'.$settings['default_images_url'].'/kdm_up.png" /><img src="'.$settings['default_images_url'].'/kdm_down.png" />';
-$txt['karmadelete'] = 'Удалять карму у пользователей, если удаляется ее описание в логе кармы';
+$txt['karmabuttons'] = 'Показывать небольшие кнопки вместо текстовых названий изменения кармы <img src="' . $settings['default_images_url'] . '/kdm_up.png" /><img src="' . $settings['default_images_url'] . '/kdm_down.png" />';
+$txt['karmapictureinlog'] = 'Показывать небольшое изображение в логе кармы, вместо плюсов и минусов <img src="' . $settings['default_images_url'] . '/kdm_up.png" /><img src="' . $settings['default_images_url'] . '/kdm_down.png" />';
+$txt['karmadelete'] = 'Удалять карму у пользователя, если удаляется ее описание в логе кармы';
 $txt['karmalabellink'] = 'Использовать метку кармы в сообщениях пользователя как ссылку на описание изменений его кармы';
 $txt['karmalastchange'] = 'Показывать последнее изменение кармы на главной странице';
 $txt['karmalastchangenum'] = 'Количество изменений, которые будут показаны на главной странице';
@@ -52,16 +47,12 @@ $txt['karmadescfieldnum'] = 'Количество символов, отображаемых в описании измене
 $txt['karmaremain'] = 'Количество кармы, которое пользователь может поставить за сутки <div class="smalltext">0 = без ограничений</div>';
 $txt['karmainmessage'] = 'Отображать в сообщениях пользователей количество кармы, которое они получили за данное сообщение';
 $txt['karmainmessagelink'] = 'Позволить просматривать изменения кармы за конкретное сообщение. В каждом сообщении появляется ссылка';
-]]></add>
-		</operation>
-	</file>
 
+// Who
+$txt['whoadmin_viewkarma'] = 'Просматривает <a href="' . $scripturl . '?action=viewkarma">лог кармы</a>.';
+$txt['whoall_modifykarma'] = 'Изменяет карму.';
 
-	<file name="$themedir/languages/Help.russian.php" error="skip">
-		<operation>
-			<search position="end"></search>
-			<add><![CDATA[
-//Karma Descriprion Mod
+// Help
 $helptxt['karmapermiss'] = 'По умолчанию эта опция выключена и лог кармы может посмотреть только администратор форума. Если Вы хотите, чтобы лог кармы был доступен всем остальным, включите эту опцию и раздайте права группам в разделе Права пользователей.';
 $helptxt['karmaotherstat'] = 'Отображение остальной статистики, в общем логе кармы.';
 $helptxt['karmalinks'] = 'Если Вы включите данную опцию, пользователи увидят в своих профилях две ссылки. ОНи могут посмотреть кому они изменили карму и кто им ее изменил. Мод должен быть включен.';
@@ -69,7 +60,7 @@ $helptxt['karmakarma'] = 'Данная опция отображает в логе кармы кол-во плюсов и м
 $helptxt['karmaurl'] = 'Данная опция позволяет увидеть в логе кармы ссылку на сообщение, в котором была изменена карма.';
 $helptxt['karmaisowner'] = 'Если Вы включите данную опцию, ссылки в профиле на карму увидит только сам обладатель профиля. Общий лог кармы будет недоступен.';
 $helptxt['karmanotifier'] = 'Если Вы включите данную опцию, пользователи смогут назначить уведомление об изменении их кармы. Изменяется это все в настройках профиля.';
-$helptxt['karmaidmember'] = 'Если Вы включили функцию уведомления изменения кармы и Ваши пользователи выбрали пунк уведомления \"Личное сообщение\" они получат личное сообщение от имени пользователя, чей ID номер Вы указали. Например, если Вы указали номер 1 (ID_MEMBER=1) пользователи получат сообщение от Вас, если Вы первый пользователь на форуме. Если укажете id = 0 сообщение прийдет от Admin-Гость.';
+$helptxt['karmaidmember'] = 'Если Вы включили функцию уведомления изменения кармы и Ваши пользователи выбрали пункт уведомления \"Личное сообщение\" они получат личное сообщение от имени пользователя, чей ID номер Вы указали. Например, если Вы указали номер 1 (ID_MEMBER=1) пользователи получат сообщение от Вас, если Вы первый пользователь на форуме. Если укажете id = 0 сообщение прийдет от Admin-Гость.';
 $helptxt['karma_pm_send_link'] = 'Отправляет ссылку на личный лог кармы.';
 $helptxt['karma_pm_send_desc'] = 'Отправляет объяснение за что была изменена карма.';
 $helptxt['karma_pm_send_changelink'] = 'Отправляет ссылку на сообщение, в котором была изменена карма.';
@@ -77,15 +68,7 @@ $helptxt['karmacantmodify'] = 'Через запятую, без пробелов.';
 $helptxt['karmacantmodify2'] = 'Через запятую, без пробелов.';
 $helptxt['karmaremain'] = '0 - без ограничений';
 
-]]></add>
-		</operation>
-	</file>
-
-	<file name="$themedir/languages/index.russian.php" error="skip">
-		<operation>
-			<search position="end"></search>
-			<add><![CDATA[
-//Karma Description Mod
+// index
 $txt['karmaview'] = 'Просмотр изменения кармы пользователей';
 $txt['whykarmamod'] = 'Изменение кармы';
 $txt['Description'] = 'Нет описания';
@@ -126,59 +109,39 @@ $txt['karma_notifier'] = 'Внимание! Ваша карма была изменена ';
 $txt['karma_notifier2'] = ' раз(а). Вы хотите посмотреть лог кармы?';
 $txt['karma_pm_subject'] = 'Уведомление кармы';
 $txt['karma_pm_body'] = 'Здравствуйте! Ваша карма была изменена. ';
-$txt['karma_pm_body2'] = 'Вы можете просмотреть лог по этой ссылке:<br />'.$scripturl.'?action=ownkarma;u='.'';
+$txt['karma_pm_body2'] = 'Вы можете просмотреть лог по этой ссылке:<br />' . $scripturl . '?action=ownkarma;u=' . '';
 $txt['karma_pm_send_desc'] = 'Отправлять объяснение изменения кармы в личном сообщении';
 $txt['karma_pm_send_desc2'] = '<br /><br />Объяснение было следующим: ';
 $txt['karma_pm_send_changelink'] = '<br /><br />Ваша карма была изменена в этом сообщении:<br />';
-$txt['karma_last_change']='Последнее изменение кармы';
-$txt['karma_last_change1']='Последние ';
-$txt['karma_last_change2']='  изменений кармы';
-$txt['karma_remain']='У вас осталось изменений: ';
-$txt['karma_remain_admin']='&infin;';
-$txt['karma_count']=' к карме за данное сообщение';
+$txt['karma_last_change'] = 'Последнее изменение кармы';
+$txt['karma_last_change1'] = 'Последние ';
+$txt['karma_last_change2'] = '  изменений кармы';
+$txt['karma_remain'] = 'У вас осталось изменений: ';
+$txt['karma_remain_admin'] = '&infin;';
+$txt['karma_count'] = ' к карме за данное сообщение';
 
-$txt['karmamessage']='Просмотр изменения кармы за конкретное сообщение';
-$txt['karmamessage_executor']='Изменил';
-$txt['karmamessage_description']='Описание изменения';
-$txt['karmamessage_date']='Дата изменения';
-$txt['karmamessage_what']='Изменение';
-$txt['karmamessage_author']='Сообщение';
-]]></add>
-		</operation>
-	</file>
+$txt['karmamessage'] = 'Просмотр изменения кармы за конкретное сообщение';
+$txt['karmamessage_executor'] = 'Изменил';
+$txt['karmamessage_description'] = 'Описание изменения';
+$txt['karmamessage_date'] = 'Дата изменения';
+$txt['karmamessage_what'] = 'Изменение';
+$txt['karmamessage_author'] = 'Сообщение';
 
-	<file name="$themedir/languages/ManagePermissions.russian.php" error="skip">
-		<operation>
-			<search position="end"></search>
-			<add><![CDATA[
-//Karma Description Mod
+// ManagePermissions
 $txt['permissionname_karmalog_view'] = 'Просмотр Лога Кармы';
 $txt['permissionhelp_karmalog_view'] = 'Лог Кармы показывает пользователям изменения и описания за что была изменена карма. Данное разрешение будет работать только тогда, когда вы включите мод кармы в настройках форума. На главной странице форума появляется ссылка для просмотра.';
-]]></add>
-		</operation>
-	</file>
 
-	<file name="$themedir/languages/Errors.russian.php" error="skip">
-		<operation>
-			<search position="end"></search>
-			<add><![CDATA[
-//Karma Description Mod
+// Errors
 $txt['cannot_karmalog_view'] = 'Извините - у Вас нет права просмотра лога кармы.';
 $txt['viewkarma_error'] = 'ID пользователя должен иметь числовой тип';
 $txt['karma_not_topic_starter'] = 'Извините, Вы можете изменить карму только автору темы.';
 $txt['karma_cant_modify'] = 'Извините, Вы не можете изменить карму этого пользователя.';
 $txt['karma_cant_modify2'] = 'Извините, Вы не можете изменять карму другим пользователям.';
 $txt['kdm_error'] = 'Karma Description Mod выключен';
-global $context;
-$txt['kdm_remain_error'] = 'Извините, на сегодня Ваш лимит изменений кармы закончился. Всего разрешено сделать '.isset($modSettings['karmaremain']).' изменений за 24 часа. <br>Следующее изменение будет доступно '.isset($context['nextKarma']).'';
-]]></add>
-		</operation>
-	</file>
 
-	<file name="$themedir/languages/Profile.russian.php" error="skip">
-		<operation>
-			<search position="end"></search>
-			<add><![CDATA[
+$txt['kdm_remain_error'] = 'Извините, на сегодня Ваш лимит изменений кармы закончился. Всего разрешено сделать ' . isset($modSettings['karmaremain']) . ' изменений за 24 часа. <br>Следующее изменение будет доступно ' . isset($context['nextKarma']) . '';
+
+// Profile
 $txt['who_change_my_karma'] = 'Кто изменил мою карму';
 $txt['whom_i_change_karma'] = 'Кому я изменил карму';
 $txt['enable_notify'] = 'Уведомлять меня, когда моя карма будет изменена';
@@ -186,7 +149,3 @@ $txt['enable_notify_none'] = 'не уведомлять';
 $txt['enable_notify_popup'] = 'Всплывающее окно';
 $txt['enable_notify_pm'] = 'Личное сообщение';
 $txt['display_karma_settings'] = 'Изменить метод уведомления при изменении кармы';
-]]></add>
-		</operation>
-	</file>
-</modification>
